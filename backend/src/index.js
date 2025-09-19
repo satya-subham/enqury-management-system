acc4f15e800593bcd7a357ccff7076b81a4c3444
satya-subham <satyasubham85@gmail.com>
2025-09-19 18:53:56 +0530

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 import CORS from "cors";
 import cookieParser from "cookie-parser";
 
+import path from 'path';
+
 import databaseConnection from "./config/databaseConnect.js";
 import authRoutes from "./routes/auth.routes.js";
 import inquriesRoutes from "./routes/inquries.routes.js";
 import usersRoutes from "./routes/users.routes.js";
 
 const app = express();
+const __dirname = path.resolve();
 dotenv.config();
 app.use(
   CORS({
@@ -26,6 +29,12 @@ app.use("/api/users", usersRoutes);
 app.use("/api/health", (req, res) => {
   res.send("Welcome to the health check api");
 });
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist'/ 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   databaseConnection(process.env.DB_URL);
