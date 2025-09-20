758ebdb6087b4eafee02720faec9c86e8e33de91
satya-subham <satyasubham85@gmail.com>
2025-09-20 13:44:02 +0530

    route

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.use("/api/health", (req, res) => {
 });
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'dist'/ 'index.html'));
   });
 }
