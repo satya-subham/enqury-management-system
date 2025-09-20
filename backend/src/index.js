10cbef5a9c4c9007e2d93f8f8704c54c274a9b66
satya-subham <satyasubham85@gmail.com>
2025-09-20 13:56:51 +0530

    code change

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,11 +34,12 @@ app.use("/api/health", (req, res) => {
 
 // Serve frontend in production (only if fullstack on one server)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
   });
 }
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
